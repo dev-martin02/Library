55826a18a7203e5fdf27a5bf6dff851fe63259fa
Web-Martin <martin052017@outlook.com>
2024-05-16 08:22:18 -0400

    Store were added it, plus some real books too!!

---
+++ frontend/store/store.jsx
@@ -0,0 +1,8 @@
+/*
+    - Fix the name of the store File 
+    - What can I put in the store? 
+        - Loading state 
+        - User state 
+        - Store the data from one fetch, and use it in multiples files
+
+*/
