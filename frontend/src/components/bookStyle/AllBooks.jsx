1e3ca183a8510b309e664716954cacc2cf6ffbf4
Web-Martin <martin052017@outlook.com>
2024-05-17 18:14:52 -0400

    Desktop desing was improve for allBooks.jsx file

--- frontend/src/components/bookStyle/AllBooks.jsx
+++ frontend/src/components/bookStyle/AllBooks.jsx
@@ -1,30 +1,27 @@
 import { Link } from "react-router-dom";
+
 export default function AllBooks({ book, index }) {
   return (
     <div
-      className="flex ring-2 ring-slate-900/5 m-1 rounded-md overflow-hidden shadow-xl"
+      className="flex flex-col md:flex-row ring-2 ring-slate-900/5 m-1 rounded-md overflow-hidden shadow-xl md:w-full p-3"
       key={index}
     >
       <img
-        className="h-48 border-none"
+        className="h-48 w-full object-scale-down md:h-full md:w-48"
         src={`../../../public/images/${book.imageName}`} //Why has to be in the public folder?
         alt="book picture"
       />
-      <div className="p-2">
-        <h2 className="font-semibold text-xl">{book.bookName}</h2>
-        <p>{`by ${book.author}`}</p>
-        <p>Genre ↓</p>
-        <ul className="flex space-x-2">
-          <li className="ring-2 rounded-md px-1">{book.genre}</li>
-        </ul>
-        <div className="flex justify-between space-x-28">
-          <div>
-            <p>Rating</p>
-            <p>4.5 stars</p>
-          </div>
+      <div className="p-2 flex flex-col justify-between md:flex-1">
+        <div>
+          <h2 className="font-semibold text-xl">{book.bookName}</h2>
+          <p>{`by ${book.author}`}</p>
+          <p>Genre ↓</p>
+          <ul className="flex space-x-2 mb-3">
+            <li className="ring-2 rounded-md px-1">{book.genre}</li>
+          </ul>
           <Link
             to={`/bookInfo/${book._id}`}
-            className=" bg-slate-400 rounded-md py-1 px-2 mt-2"
+            className="bg-slate-400 rounded-md py-1 px-2 mt-2"
           >
             See More
           </Link>
